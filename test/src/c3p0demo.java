5b20c4e230cf5bd38c410affb9426d4f6312a305
yangxiaoxiao <yangxiaoxiao@yangxiaoxiaoMacBook-Pro.local>
2019-02-27 16:47:25 +0800

    test

---
+++ test/src/c3p0demo.java
@@ -0,0 +1,17 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+//c3p0
+public class c3p0demo {
+    public static void main(String[] args) throws SQLException {
+        //创建数据库连接池对象
+        DataSource ds=new ComboPooledDataSource();
+        for (int i = 0; i <10 ; i++) {
+            Connection conn =ds.getConnection();
+            System.out.println(i+":"+conn);
+
+        }}
+}
